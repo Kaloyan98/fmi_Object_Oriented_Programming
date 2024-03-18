3152f5b3ec3bd67855bacac4073d99a4dab2c52b
Kaloyan98 <63058392+Kaloyan98@users.noreply.github.com>
2024-03-18 14:05:42 +0200

    Create Grade.h

---
+++ Exercises/week_04/Grade.h
@@ -0,0 +1,23 @@
+#pragma once
+
+const int MAX_LENGTH = 35;
+
+class Grade {
+private:
+	int grade;
+	char subject[MAX_LENGTH];
+public:
+	Grade();
+
+	Grade(const char* subject, int grade);
+
+	void setGrade(int grade);
+
+	int getGrade() const;
+	
+	void setSubject(const char* subject);
+
+	const char* getSubject() const;
+
+	void print() const;
+};
