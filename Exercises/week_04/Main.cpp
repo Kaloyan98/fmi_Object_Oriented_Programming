95ea3377ca5d8e682d0d37d5047c95bae72be905
Kaloyan98 <63058392+Kaloyan98@users.noreply.github.com>
2024-03-18 14:06:17 +0200

    Create Main.cpp

---
+++ Exercises/week_04/Main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "Grade.h"
+#include "Student.h"
+
+int main() {
+	Grade initialGrade;
+	Grade grade("Algebra", 5);
+
+	initialGrade.print();
+	grade.print();
+
+	Student initialStudent;
+	Student ivancho(62193, "Ivan Ivanov", 10);
+	
+	initialStudent.print();
+	ivancho.print();
+
+	return 0;
+}
