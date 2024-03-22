65b2551ee7fab6512e86d0798e6c2dbd2ffa04c4
Kaloyan98 <63058392+Kaloyan98@users.noreply.github.com>
2024-03-22 15:54:56 +0200

    Create Basket.h

---
+++ Practice/Serialization_Deserialization/Basket.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "Egg.h"
+
+const int INITIAL_CAPACITY = 2;
+
+class Basket {
+public:
+	Basket(const char* name);
+	Basket(const Basket& basket);
+	~Basket();
+	
+	Basket& operator=(const Basket& basket);
+
+	void generateReport();
+	void serialization();
+	void deserialization();
+
+	void setName(const char* name);
+	void addEgg(const Egg& egg);
+	void removeEgg(const Egg& egg);
+private:
+	void resize();
+	int getEggIndex(const char* eggName);
+private:
+	char* name;
+	Egg* eggs;
+	int count;
+	int capacity;
+};
