bc52edbbf4a6b122e121908d4cb313bace8bc351
Kaloyan98 <63058392+Kaloyan98@users.noreply.github.com>
2024-03-22 15:42:04 +0200

    Create Pencil.h

---
+++ Exercises/Exam_Prep_2/Pencil.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "WritingInstrument.h"
+
+class Pencil : public WritingInstrument {
+public:
+	int getHardness() const;
+	void setHardness(int hardness);
+private:
+	int hardness;
+};
+
+std::istream& operator>>(std::istream& is, Pencil& pencil);
+std::ostream& operator<<(std::ostream& os, const Pencil& pencil);
