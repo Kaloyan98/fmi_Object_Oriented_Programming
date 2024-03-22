7a502f950d8210a0289bbded08ec7fca9aa51325
Kaloyan98 <63058392+Kaloyan98@users.noreply.github.com>
2024-03-22 13:56:17 +0200

    Create Manager.h

---
+++ Exercises/week_08/Manager.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "Employee.h"
+
+class Manager : public Employee {
+public:
+	Manager();
+	Manager(const char* name, int experience, double salary, int employeeCount);
+
+	void setEmployeeCount(int count);
+	int getEmployeeCount() const;
+
+	void print() const;
+private:
+	int employeeCount;
+};
