0e089dec4ae78802f91b631f60ae1072022e2238
Kaloyan98 <63058392+Kaloyan98@users.noreply.github.com>
2024-03-22 13:28:36 +0200

    Create DynamicArray.h

---
+++ Exercises/week_05/03_Custom_Array/DynamicArray.h
@@ -0,0 +1,32 @@
+#pragma once
+
+const int INITIAL_CAPACITY = 2;
+
+class DynamicArray {
+private:
+	int size;
+	int capacity;
+	int* arr;
+public:
+	DynamicArray();
+	DynamicArray(int capacity);
+	DynamicArray(const DynamicArray& dynamicArray);
+	~DynamicArray();
+
+	void resize(int s);
+	void insertAt(int i, int value);
+	void pushBack(int value);
+	int popBack();
+	void removeAt(int i);
+	void clear();
+
+	bool isEmpty() const;
+	int getAt(int i) const;
+	int getCapacity() const;
+	int getSize() const;
+
+	void print() const; // only for testing
+private:
+	void shiftLeft(int i);
+	void shiftRight(int i);
+};
