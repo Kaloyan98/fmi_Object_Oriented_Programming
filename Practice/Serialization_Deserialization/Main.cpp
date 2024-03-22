6856a935d2a664c44c9ce04378f00ec43467e2c5
Kaloyan98 <63058392+Kaloyan98@users.noreply.github.com>
2024-03-22 15:56:36 +0200

    Create Main.cpp

---
+++ Practice/Serialization_Deserialization/Main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "Egg.h"
+#include "Basket.h"
+
+int main() {
+	Basket gosho("gosho");
+
+	gosho.addEgg(Egg("Jyltyka"));
+	gosho.addEgg(Egg("Beltycheto"));
+	gosho.addEgg(Egg("Smalcho"));
+	gosho.addEgg(Egg("Gigantsko"));
+	
+	gosho.removeEgg(Egg("Smalcho"));
+	
+	gosho.serialization();
+	gosho.deserialization();
+
+	gosho.generateReport();
+
+	return 0;
+}
