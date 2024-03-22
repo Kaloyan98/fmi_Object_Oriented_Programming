18ca683085891c4033d220b845c9c1b946ce9151
Kaloyan98 <63058392+Kaloyan98@users.noreply.github.com>
2024-03-22 14:01:52 +0200

    Create Company.h

---
+++ Exercises/week_09/Company.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <vector>
+
+#include "Employee.h"
+#include "Manager.h"
+
+class Company {
+public:
+	Company();
+	Company(const Company& other);
+	Company& operator=(const Company& rhs);
+	~Company();
+
+	double getTotalWork() const;
+
+	void setBoss(Manager* boss);
+	 
+	void operator+=(Employee* newEmployee);
+	void addEmployee(Employee* newEmployee);
+	void removeEmployee(int index);
+
+	double getTotalExpenses() const;
+	double getAverageSalary() const;
+	void removeNewEmployees();
+
+	void printAll() const;
+private:
+	void copyFrom(const Company& other);
+	void clear();
+private:
+	Manager* boss;
+	std::vector<Employee*> employees;
+};
