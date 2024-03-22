876b67ded4090525a4cdb329539e6650be1b7eb2
Kaloyan98 <63058392+Kaloyan98@users.noreply.github.com>
2024-03-22 15:40:04 +0200

    Create Main.cpp

---
+++ Exercises/Exam_Prep_2/Main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include "Marker.h"
+#include "Pencil.h"
+
+int main() {
+	Marker marker;
+	std::cin >> marker;
+	std::cout << marker;
+
+	Pencil pencil;
+	std::cin >> pencil;
+	std::cout << pencil;
+	return 0;
+}
