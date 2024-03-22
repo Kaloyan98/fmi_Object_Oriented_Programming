865d103a66ebb04fa05c1f71f42feb71de52ee16
Kaloyan98 <63058392+Kaloyan98@users.noreply.github.com>
2024-03-22 14:06:46 +0200

    Create Manager.h

---
+++ Exercises/week_09/Manager.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "Employee.h"
+
+class Manager : public Employee {
+public:
+	Manager();
+	Manager(const char* name, int experience, double salary, int employeeCount);
+	virtual ~Manager();
+
+	virtual double work();
+
+	void setEmployeeCount(int count);
+	int getEmployeeCount() const;
+
+	void print() const;
+private:
+	int employeeCount;
+};
