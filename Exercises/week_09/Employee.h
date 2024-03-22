a1ae9219cd324224362e9bc43032d8dba4caffe1
Kaloyan98 <63058392+Kaloyan98@users.noreply.github.com>
2024-03-22 14:03:28 +0200

    Create Employee.h

---
+++ Exercises/week_09/Employee.h
@@ -0,0 +1,27 @@
+#pragma once
+
+class Employee {
+public:
+	Employee();
+	Employee(const char* name, int experience, double salary);
+	Employee(const Employee& employee);
+	Employee& operator=(const Employee& rhs);
+	virtual ~Employee();
+
+	virtual double work();
+	virtual Employee* clone();
+
+	const char* getName() const;
+	int getExperience() const;
+	double getSalary() const;
+
+	void setName(const char* name);
+	void setExperience(int experience);
+	void setSalary(double salary);
+
+	virtual void print() const;
+private:
+	char* name;
+	int experience;
+	double salary;
+};
