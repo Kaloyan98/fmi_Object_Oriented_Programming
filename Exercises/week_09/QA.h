ced1f41dc24410591ee87ab5d140f76badd3eb44
Kaloyan98 <63058392+Kaloyan98@users.noreply.github.com>
2024-03-22 14:09:06 +0200

    Create QA.h

---
+++ Exercises/week_09/QA.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "ITSpecialist.h"
+
+class QA : public ITSpecialist {
+public:
+	QA();
+	QA(const char* name, int experience, double salary, const char* project, bool isManual);
+	virtual ~QA();
+
+	virtual double work();
+	virtual Employee* clone();
+
+
+	void setIsManual(bool isManual);
+	bool getIsManual() const;
+
+	virtual void print() const;
+private:
+	bool isManual;
+};
