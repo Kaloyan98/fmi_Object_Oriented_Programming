9f67a7a23cfba707ba1be04295c61c74a46042c0
Kaloyan98 <63058392+Kaloyan98@users.noreply.github.com>
2024-03-22 13:34:28 +0200

    Create Vector.h

---
+++ Exercises/week_06/01_Vector/Vector.h
@@ -0,0 +1,28 @@
+#pragma once
+
+class Vector {
+public:
+	Vector();
+	Vector(int dimension);
+	Vector(double* coordinates, int dimension);
+	Vector(const Vector& other);
+	~Vector();
+	Vector& operator=(const Vector& vector);
+
+	Vector operator+(const Vector& vector);
+	Vector operator-(const Vector& vector);
+	Vector operator*(double lambda);
+	double operator[](int index) const;
+	
+	int getDimension() const;
+
+	void setCoordinates(double* coordinates);
+	void setDimension(int dimension);
+private:
+	double* coordinates;
+	int dimension;
+};
+
+std::ostream& operator<<(std::ostream& os, const Vector& vector);
+
+std::istream& operator>>(std::istream& is, Vector& vector);
