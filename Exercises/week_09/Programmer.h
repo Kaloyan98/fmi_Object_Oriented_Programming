3c498088224d5d8834cd0d76b985638448e25cf0
Kaloyan98 <63058392+Kaloyan98@users.noreply.github.com>
2024-03-22 14:08:00 +0200

    Create Programmer.h

---
+++ Exercises/week_09/Programmer.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "ITSpecialist.h"
+
+class Programmer : public ITSpecialist {
+public:
+	Programmer();
+	Programmer(const char* name, int experience, double salary, const char* project, const char* languages);
+	virtual ~Programmer();
+	virtual double work();
+
+	const char* getLanguages() const;
+	void setLanguages(const char* languages);
+
+	virtual Employee* clone();
+
+	virtual void print() const;
+private:
+	char languages[20];
+};
