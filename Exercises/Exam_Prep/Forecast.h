6b0a2fb43d828b849f86190f24e70061416c3772
Kaloyan98 <63058392+Kaloyan98@users.noreply.github.com>
2024-03-22 15:36:29 +0200

    Create Forecast.h

---
+++ Exercises/Exam_Prep/Forecast.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <iostream>
+#include "Temperature.h"
+
+class Forecast {
+public:
+	Forecast();
+	Forecast(const char* place, const Temperature& temp);
+	Forecast(const Forecast& other);
+	Forecast& operator=(const Forecast& rhs);
+	~Forecast();
+
+	const char* getPlace() const;
+	const Temperature getTemperature() const;
+
+	void setPlace(const char* name);
+	void setTemperatures(int min, int avg, int max);
+
+	bool updateIfHotter(const Forecast& f);
+private:
+	char* placeName;
+	Temperature temperature;
+};
+
+std::istream& operator>>(std::istream& is, Forecast& forecast);
+std::ostream& operator<<(std::ostream& os, const Forecast& forecast);
