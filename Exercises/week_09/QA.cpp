f379fc5ecfdb5045d1d19f3a980d2472094c3f9c
Kaloyan98 <63058392+Kaloyan98@users.noreply.github.com>
2024-03-22 14:08:41 +0200

    Create QA.cpp

---
+++ Exercises/week_09/QA.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include "QA.h"
+
+QA::QA() : ITSpecialist(), isManual(false) {}
+
+QA::QA(const char * name, int experience, double salary, const char * project, bool isManual)
+	: ITSpecialist(name, experience, salary, project) {
+	this->setIsManual(isManual);
+}
+
+QA::~QA() {}
+
+double QA::work() {
+	double moneyPerMonth = this->isManual ? 55 : 40;
+	double usefulness = this->getUsefulness(moneyPerMonth);
+	std::cout << "Quality Assurance " << this->getName() << " usefulness ~ " << usefulness << "\n";
+	return usefulness;
+}
+
+Employee* QA::clone() {
+	return new QA(*this);
+}
+
+void QA::setIsManual(bool isManual) {
+	this->isManual = isManual;
+}
+
+bool QA::getIsManual() const {
+	return this->isManual;
+}
+
+void QA::print() const {
+	std::cout << "QA specialist: \n";
+	ITSpecialist::print();
+	std::cout << "----Is manual testing? "
+		<< (this->isManual ? "Yes" : "No") << "\n";
+}
