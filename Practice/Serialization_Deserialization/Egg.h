3daac1de09d560f7681acbe79b85a4f5ac4fdf66
Kaloyan98 <63058392+Kaloyan98@users.noreply.github.com>
2024-03-22 15:56:11 +0200

    Create Egg.h

---
+++ Practice/Serialization_Deserialization/Egg.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <iostream>
+
+class Egg {
+public:
+	Egg();
+	Egg(const char* name);
+	Egg(const char* name, int size);
+	Egg(const Egg& egg);
+	~Egg();
+	Egg& operator=(const Egg& egg);
+	
+	void eggPrint(std::ofstream& os) const;
+	void eggSerialization(std::ofstream& os) const;
+	void eggDeserialization(std::ifstream& is);
+
+	const char* getName() const;
+	int getSize() const;
+
+	void setName(const char* name);
+private:
+	char* name;
+	int size;
+};
