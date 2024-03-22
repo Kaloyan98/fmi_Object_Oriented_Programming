01f949992e9b5342aeda5a2525e152cf7f4dbb47
Kaloyan98 <63058392+Kaloyan98@users.noreply.github.com>
2024-03-22 13:35:49 +0200

    Create Main.cpp

---
+++ Exercises/week_06/02_Matrix/Main.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include "Matrix.h"
+
+double** createMatrix(int n) {
+	double** matrix = new double*[n];
+	for (int i = 0; i < n; i++) {
+		matrix[i] = new double[n];
+	}
+
+	return matrix;
+}
+
+void deleteMatrix(double** arr, int n) {
+	std::cout << "Cleaning up memory... ";
+	for (int i = 0; i < n; i++) {
+		delete[] arr[i];
+	}
+	delete[] arr;
+	std::cout << "Done!\n";
+}
+
+int main() {
+	/*
+	Matrix m;
+	std::cin >> m;
+	std::cout << m;
+	*/
+
+	// int size = 6;
+	int size = 3;
+	int counter = 1;
+	double** matrixA = createMatrix(size);
+	double** matrixB = createMatrix(size);
+
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			matrixA[i][j] = counter++;
+			matrixB[i][j] = counter * 2;
+		}
+	}
+
+	Matrix a(matrixA, size);
+	Matrix b(matrixB, size);
+
+	std::cout << "Matrix a:\n" << a;
+	std::cout << "Matrix b:\n" << b;
+
+	Matrix aPlusB = a + b;
+	std::cout << "a + b = \n" << aPlusB;
+
+	Matrix bMinusA = b - a;
+	std::cout << "b - a = \n" << bMinusA;
+
+	Matrix aMultiplyB = a * b;
+	std::cout << "a * b = \n" << aMultiplyB;
+	
+	Matrix aMultiplyLambda = a * 5;
+	//std::cout << "a * 5 = :\n" << aMultiplyLambda;
+
+	int simpleMatrixSize = 2;
+	double** simpleMatrix = createMatrix(simpleMatrixSize);
+	simpleMatrix[0][0] = 4;
+	simpleMatrix[0][1] = 3;
+	simpleMatrix[1][0] = 3;
+	simpleMatrix[1][1] = 2;
+
+	Matrix matrixC(simpleMatrix, simpleMatrixSize);
+	std::cout << "Matrix C = \n" << matrixC;
+	
+	double determinant = *matrixC;
+	std::cout << "Determinant of matrix C = " << determinant << "\n";
+	
+	Matrix inverse = !matrixC;
+	std::cout << "Inverse matrix of matrix C:\n" << inverse;
+
+	deleteMatrix(matrixA, size);
+	deleteMatrix(matrixB, size);
+	deleteMatrix(simpleMatrix, simpleMatrixSize);
+	
+	return 0;
+}
