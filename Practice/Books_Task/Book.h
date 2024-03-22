416d373e066eca0981afe2152f791670fa17373a
Kaloyan98 <63058392+Kaloyan98@users.noreply.github.com>
2024-03-22 15:59:04 +0200

    Create Book.h

---
+++ Practice/Books_Task/Book.h
@@ -0,0 +1,25 @@
+#pragma once
+
+class Book {
+public:
+	Book();
+	Book(const char* title, int pages);
+	Book(const Book& other);
+	Book& operator=(const Book& rhs);
+	~Book();
+
+	bool operator<(const Book& rhs);
+	bool operator>(const Book& rhs);
+
+	void setTitle(const char* title);
+	void setPages(int pages);
+
+	const char* getTitle() const;
+	int getPages() const;
+private:
+	char* title;
+	int pages;
+};
+
+std::istream& operator>>(std::istream& is, Book& book);
+std::ostream& operator<<(std::ostream& os, const Book& book);
