57dc3c0633eb24816640fe58bdff598d45071a6f
Kaloyan98 <63058392+Kaloyan98@users.noreply.github.com>
2024-03-23 11:46:10 +0200

    Create MathExpression.h

---
+++ Practice/Math_Expressions/MathExpression.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <iostream>
+
+const int MAX_LENGTH = 10;
+
+template<typename T>
+class MathExpression {
+public:
+	virtual ~MathExpression();
+	
+	virtual MathExpression<T>* clone() const = 0;
+
+	virtual T value() const = 0;
+
+	virtual void print() const;
+};
+
+template<typename T>
+inline MathExpression<T>::~MathExpression() {}
+
+template<typename T>
+inline void MathExpression<T>::print() const {
+	std::cout << this->value() << "\n";
+}
