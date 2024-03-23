fb5f0e5ed839580d2e0d7298cb3158ef2b2a9086
Kaloyan98 <63058392+Kaloyan98@users.noreply.github.com>
2024-03-23 11:42:12 +0200

    Create Library.h

---
+++ Practice/Books_Task/Library.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "Book.h"
+
+const int DEFAULT_CAPACITY = 2;
+
+class Library {
+public:
+	Library();
+	Library(const Library& other);
+	Library& operator=(const Library& rhs);
+	~Library();
+
+	Book operator[](int index) const;
+	int getSize() const;
+
+	void addBook(const Book& book);
+	void removeBook(int index);
+	void removeBook(const char* name);
+
+	void sortByTitle();
+private:
+	void resize(int newCapacity);
+	void copyFrom(const Library& other);
+	void clear();
+private:
+	int size;
+	int capacity;
+	Book* books;
+};
+
+std::ostream& operator<<(std::ostream& os, const Library& library);
