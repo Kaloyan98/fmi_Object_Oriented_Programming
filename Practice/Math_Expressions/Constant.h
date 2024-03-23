ef023ccb3f968ecb23cbfef9b6148fe892a156a3
Kaloyan98 <63058392+Kaloyan98@users.noreply.github.com>
2024-03-23 11:45:17 +0200

    Create Constant.h

---
+++ Practice/Math_Expressions/Constant.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "MathExpression.h"
+
+template<typename T>
+class Constant : public MathExpression<T> {
+public:
+	Constant(const T& value);
+
+	virtual MathExpression<T>* clone() const override;
+
+	virtual T value() const override;
+private:
+	T v;
+};
+
+template<typename T>
+inline MathExpression<T>* Constant<T>::clone() const {
+	return new Constant<T>(*this);
+}
+
+template<typename T>
+inline Constant<T>::Constant(const T& value) {
+	this->v = value;
+}
+
+template<typename T>
+inline T Constant<T>::value() const {
+	return this->v;
+}
