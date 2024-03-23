706f9ded547436872695ce24e895fdb25e8a1c5a
Kaloyan98 <63058392+Kaloyan98@users.noreply.github.com>
2024-03-23 11:45:36 +0200

    Create Main.cpp

---
+++ Practice/Math_Expressions/Main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+#include "Average.h"
+#include "Constant.h"
+#include "Max.h"
+#include "Sum.h"
+#include "Prod.h"
+
+int main() {
+	MathExpression<int>* numberFive = new Constant<int>(5);
+	MathExpression<int>* numberTen = new Constant<int>(10);
+
+	Max<int>* max = new Max<int>();
+	max->addExpression(numberFive);
+	max->addExpression(numberTen);
+	max->print();
+
+	Average<int>* average = new Average<int>();
+	average->addExpression(numberFive);
+	average->addExpression(numberTen);
+	average->print();
+
+	Sum<int>* sum = new Sum<int>(max, average);
+	sum->print();
+
+	Prod<int>* product = new Prod<int>(max, average);
+	product->print();
+
+	Max<int> copyMax(*max);
+	Max<int> assignmentMax = copyMax;
+	assignmentMax.print();
+
+	Average<int> copyAverage(*average);
+	Average<int> assignmentAvg = copyAverage;
+	assignmentAvg.print();
+
+	delete numberFive;
+	delete numberTen;
+	delete max;
+	delete average;
+	delete sum;
+	delete product;
+
+	return 0;
+}
