e003c90570ee4b75baf481ca5d76df859a336128
Kaloyan98 <63058392+Kaloyan98@users.noreply.github.com>
2024-03-25 18:26:47 +0200

    Create Expression.h

---
+++ Exams/Final/1/Expression.h
@@ -0,0 +1,11 @@
+#pragma once
+
+class Expression {
+public:
+	virtual ~Expression() {}
+
+	virtual Expression* clone() const = 0;
+
+	virtual double value() const = 0;
+	virtual void print() const = 0;
+};
