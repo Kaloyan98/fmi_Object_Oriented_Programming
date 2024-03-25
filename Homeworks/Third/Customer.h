5e60fc9ba9a0c3364c2cf617302461208175cb5a
Kaloyan98 <63058392+Kaloyan98@users.noreply.github.com>
2024-03-25 18:10:59 +0200

    Create Customer.h

---
+++ Homeworks/Third/Customer.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+
+class Customer {
+public:
+	Customer(long long id, std::string name, std::string address);
+
+	void display() const;
+
+	long long getId() const;
+	std::string getName() const;
+	std::string getAddress() const;
+private:
+	long long id;
+	std::string name;
+	std::string address;
+};
