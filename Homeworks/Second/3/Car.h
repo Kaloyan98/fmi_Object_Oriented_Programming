fef8913216c67f7247a997fa328d3553c79d2241
Kaloyan98 <63058392+Kaloyan98@users.noreply.github.com>
2024-03-25 12:18:51 +0200

    Create Car.h

---
+++ Homeworks/Second/3/Car.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "Vehicle.h"
+
+class Car : public Vehicle {
+public:
+	Car();
+	Car(const char* make, const char* model, Color color, int year, int mileage);
+	virtual ~Car();
+
+	virtual void details() const override;
+};
