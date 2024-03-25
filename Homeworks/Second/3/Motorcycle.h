ae4bb9c19c80f061afedce6fcbca3c4f2ff6d902
Kaloyan98 <63058392+Kaloyan98@users.noreply.github.com>
2024-03-25 12:21:50 +0200

    Create Motorcycle.h

---
+++ Homeworks/Second/3/Motorcycle.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "Vehicle.h"
+
+class Motorcycle : public Vehicle {
+public:
+	Motorcycle();
+	Motorcycle(const char* make, const char* model, Color color, int year, int mileage, const char* type);
+	Motorcycle(const Motorcycle& other);
+	Motorcycle& operator=(const Motorcycle& rhs);
+	virtual ~Motorcycle();
+
+	const char* getType() const;
+	void setType(const char* type);
+
+	virtual void details() const override;
+private:
+	char* type;
+};
