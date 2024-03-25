676fa085e9b0cdf75c5b674d3d73c06e9a1d0adb
Kaloyan98 <63058392+Kaloyan98@users.noreply.github.com>
2024-03-25 11:52:26 +0200

    Create Main.cpp

---
+++ Practice/Devices/Main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#include "Device.h"
+#include "Laptop.h"
+
+int main() {
+    Laptop lenovo;
+    Laptop macbook("Apple", 3);
+
+    Laptop macFromChina;
+    macFromChina = macbook;
+
+    macFromChina.print();
+
+    return 0;
+}
